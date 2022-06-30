f6b4bb6fbdbeda21ff93977dcbda01e0360638f2
JuanColmegna <juanfmcolmegna@gmail.com>
2022-06-29 22:55:14 -0300

    se creo navbar responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ return (
             <p className='logo'><span className='logo-color'>&lt;</span>Sistemas-SF<span className='logo-color'>/&gt;</span></p>
             <ul className='list'>
                 <li className='list-item'><button className='list-ref' href="#">Inicio</button></li>
-                <li className='list-item'><button className='list-ref' href="#">Tienda</button></li>
+                <li className='list-item'><button className='list-ref' href="#">Mi perfil</button></li>
                 <li className='list-item'><button className='list-ref' href="#">Novedades</button></li>
             </ul>
         </nav>
